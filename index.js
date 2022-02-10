e57c0b9a9d30ec6a8d382aace59a06eff086e26d
Miguel(MgARL) <mgarlcr@gmail.com>
2022-02-10 18:39:53 -0500

    Adding Places Controller

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const { listen } = require('express/lib/application')
 const app = express()
 const portNumber = process.env.PORT
 
+app.use('/places', require('./controllers/places'))
+
 app.get('/', (req,res)=>{
     res.send('Hello World')
 })
 
 app.get('*',(req, res)=>{
-    res.send('<h1>404 Page not found</h1>')
+    res.status(404).send('<h1>404 Page not found</h1>')
 })
 
 listen(portNumber, () =>{
