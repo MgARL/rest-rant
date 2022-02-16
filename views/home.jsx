8cf78eba9bd2720b40f53e765d2aaffda7fda6ba
Miguel(MgARL) <mgarlcr@gmail.com>
2022-02-16 10:17:27 -0500

    Adding and rendereing new view

--- views/home.jsx
+++ views/home.jsx
@@ -5,16 +5,18 @@ function Home(){
    return( 
    <Def>
         <main>
-            <h1>HOME</h1>
+            <h1>REST-Rant</h1>
             <div>
-                <img src="/images/madie-hamilton-steak.jpg" alt="Madie Hamilton Steak" id='steak'/>
+                <img src="/images/madie-hamilton-steak.jpg" alt="tomahawk Steak" id='steak'/>
                 <div>
                 Photo by <a href="https://unsplash.com/@mham3816?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Madie Hamilton</a> on <a href="https://unsplash.com/s/photos/steak?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
                 </div>
             </div>
-            <a href="/places">
-                <button className='btn-primary'>Places Page</button>
-            </a>
+            <div className="row">
+                <a href="/places">
+                    <button className='btn-primary'>Places Page</button>
+                </a>
+            </div>
         </main>
     </Def>
    )
